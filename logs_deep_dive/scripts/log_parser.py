163ea2f0b8eb646a574a8cd0392b1fb1800f4b45
Ghaby-X <gabrielanyaele21@gmail.com>
2025-06-10 17:17:50 +0000

    add log parser

---
+++ logs_deep_dive/scripts/log_parser.py
@@ -0,0 +1,34 @@
+import re
+import ipaddress
+from datetime import datetime, timedelta
+
+# function to get unique ips
+def extract_ip(line: list[str]):
+    if len(line) < 2: return False
+    current_ip = line[1]
+    try:
+        ipaddress.ip_address(current_ip)
+    except:
+        return False
+    
+    return current_ip
+
+
+if __name__ == "__main__":
+    unique_ip_count = {}
+    filename = "sample.log"
+
+    with open(filename) as f:
+        for line in f:
+            cur_log = line.strip()
+            line_array = line.split()
+
+            cur_ip = extract_ip(line_array)
+
+            if cur_ip:
+                if cur_ip in unique_ip_count:
+                    unique_ip_count[cur_ip] += 1
+                else:
+                    unique_ip_count[cur_ip] = 1
+    
+    print(unique_ip_count)
